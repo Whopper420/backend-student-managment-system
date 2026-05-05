17d3fb64d5928f48da2ca5756f4387ecae3c64d5
Pepsik Men <you@example.com>
2026-05-05 12:44:33 +0300

    added ability to add courses and also add students to courses on the frontend not just the backend

--- SimpleApp.Shared/obj/Debug/net8.0/SimpleApp.Shared.AssemblyInfo.cs
+++ SimpleApp.Shared/obj/Debug/net8.0/SimpleApp.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleApp.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6525967f1d16a61ffa6da159ae73063e21267152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e20c4ca14c84672dec967250424f925410e75dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleApp.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleApp.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

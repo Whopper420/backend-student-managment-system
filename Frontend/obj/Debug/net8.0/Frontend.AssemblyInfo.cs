f7b3d25fb90fd1a98efdc07c82947cedafb00036
Pepsik Men <you@example.com>
2026-05-06 09:45:54 +0300

    Revert "replaced mySQL with postrgeSQL"
    This reverts commit a317832f8fec423ad19b777fe3816b6e975cb5b6.

--- Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net8.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d3fb64d5928f48da2ca5756f4387ecae3c64d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e20c4ca14c84672dec967250424f925410e75dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
